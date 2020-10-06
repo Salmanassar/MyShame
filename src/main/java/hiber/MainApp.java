5129e1db4617de6ff7d90bfb8ad3f0e75ec576fe
Salmanassar <folknerolek@gmail.com>
2020-10-07 01:06:02 +0200

    Uknown entity name hiber.model.User

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,46 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
+
+        Car audi = new Car("audi", 4);
+        Car bmw = new Car("bmw", 6);
+        Car mercedes = new Car("mercedes", 5);
+        Car volvo = new Car("volvo", 3);
+
+       users.get(0).setCar(audi);
+       users.get(1).setCar(bmw);
+       users.get(2).setCar(mercedes);
+       users.get(3).setCar(volvo);
+
+        context.close();
+
+    }
+}
